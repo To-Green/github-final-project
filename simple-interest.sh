8ee6c8477bc375426c1da5891ba0683f292b0f06
Tohwanda Green <97702676+To-Green@users.noreply.github.com>
2022-08-24 16:31:52 -0400

    Update simple-interest.sh
    update authors

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,7 +5,7 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors: To-Green Green
+   # Additional Authors: To-Green
    
    # Input:
    # p, principal amount
